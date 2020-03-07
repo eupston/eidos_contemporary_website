dd1a10ccc622c8628d15d5f7318d94301af593b1
eupston <eupston130@hotmail.com>
2020-03-08 11:56:01 +1300

    #11 updated post login with new ErrorResponse middleware

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,54 +2,45 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
 
 // @desc    Logs user in
 // @route   GET /api/v1/auth/login
 // @access  PUBLIC
-exports.postLogin = async (req, res, next) => {
+exports.postLogin = asyncHandler(async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     let loadedUser;
-    try {
-        const user = await User.findOne({email: email});
-        console.log(user);
-        if (!user) {
-            return res.status(401)
+    const user = await User.findOne({email: email});
+    console.log(user);
+    if (!user) {
+        return next(
+            new ErrorResponse("Email Not Found",
+                404)
+        );
+    }
+    else {
+        loadedUser = user;
+        const doMatch = await bcrypt.compare(password, user.password);
+
+        if (doMatch) {
+            const token = jwt.sign(
+                {
+                    email: loadedUser.email,
+                    userId: loadedUser._id.toString()
+                },
+                'secret', {expiresIn: '1h'}
+            );
+            return res.status(200)
                 .json({
-                    success: false,
-                    data: "invalid email"
+                    token: token,
+                    userId: loadedUser._id.toString()
                 });
-        }
-        else {
-            loadedUser = user;
-            bcrypt
-                .compare(password, user.password)
-                .then(doMatch => {
-                    if (doMatch) {
-                        const token = jwt.sign({
-                            email: loadedUser.email,
-                            userId: loadedUser._id.toString()
-                        }, 'secret', {expiresIn:'1h'});
-                        return res.status(200)
-                                .json({
-                                    token: token,
-                                    userId:loadedUser._id.toString()
-                                 });
-                    }
-                    else {
-                        return res.status(401)
-                            .json({
-                                success: false,
-                                data: "invalid password"
-                            });
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+        } else {
+            return next(
+                new ErrorResponse("invalid password",
+                    401)
+            );
         }
     }
-    catch (err){
-        res.status(400).json({ success: false, data:err });
-    }
-};
+});
