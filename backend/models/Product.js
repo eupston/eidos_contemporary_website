ca6f3d5b8588f870183fff3fc310438d23767700
eupston <eupston130@hotmail.com>
2020-03-07 21:51:54 +1300

    #14 updated with Product Schema

---
+++ backend/models/Product.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        unique: true,
+        trim: true,
+        maxlength: [60, 'Name cannot be longer than 60 characters']
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    quantity : {
+        type: Number,
+        required: false,
+
+    },
+    materials: {
+      type: String,
+        required: false,
+    },
+    description : {
+        type: String,
+        required: true,
+    },
+    display_on_site :{
+      type: Boolean,
+      required: true
+    },
+    customizable: {
+        type: Boolean,
+        required: true
+    },
+    notes: {
+      type: String,
+        required: false
+    },
+    views: {
+        type: Number,
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+
+
+module.exports = mongoose.model('Product', ProductSchema);
