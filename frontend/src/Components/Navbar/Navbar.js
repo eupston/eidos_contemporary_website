6734f517406cf5a732d5eb06b21b7e2c0cdba29c
eupston <eupston130@hotmail.com>
2020-03-22 17:43:47 +1300

    #47 update with sticky navbar and other positioning

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -7,43 +7,82 @@ import {connect} from 'react-redux';
 
 class StickyNavbar extends Component {
     state = {
-
+        isHovered : false,
+        isScrolling: false,
+        navBarClasses: [classes.MainBar]
     };
+
+    handleStickyNavbar = () => {
+        const page_position = window.pageYOffset;
+
+        if ( page_position >= 50) {
+            this.setState({navBarClasses:[classes.MainBar, classes.MainBarSticky], isScrolling:true})
+        }
+        else {
+            this.setState({navBarClasses:[classes.MainBar], isScrolling:false});
+        }
+    };
+
+    componentDidMount(){
+        window.addEventListener('scroll', this.handleStickyNavbar);
+    }
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.handleStickyNavbar);
+    }
+
+    handleOnHover = () => {
+        this.setState({isHovered: true});
+    };
+
+    handleOffHover = () => {
+        this.setState({isHovered: false});
+    };
+
+
     vendorDropDowns = Object.keys(this.props.jewelers.vendors).map(ven => {
         const vendorURL = ven.toLowerCase().replace(" ", "-");
         const completeURL = "/jewelry/" + vendorURL;
-        return <Link className="dropdown-item" to={completeURL}>{ven}</Link>
+        return <Link className="dropdown-item " to={completeURL}>{ven}</Link>
     });
     render() {
+        console.log(this.state)
         return (
-            <div className={classes.Navbar} >
-                {console.log(this.props.jewelers.vendors)}
-                <br></br>
-                <img src={eidosLogo}></img>
-                <nav className="navbar navbar-expand-lg navbar-light">
-                    <Link to="/">Home</Link>
-                    <li className="nav-item dropdown">
-
-                        <Link className="nav-link dropdown-toggle"
-                              href="/"
-                              id="navbarDropdownMenuLink"
-                              to="/"
-                              data-toggle="dropdown"
-                              aria-haspopup="true"
-                              aria-expanded="false">Jewelery</Link>
-
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            {this.vendorDropDowns}
+                 <div className={this.state.navBarClasses.join(' ') } id="mainnavbar">
+                    {!this.state.isScrolling ? <img src={eidosLogo} ></img> : null}
+                    <nav className={classes.Navbar}  >
+                        <div className={classes.NavbarLeft}>
+                            {this.state.isScrolling ? <img src={eidosLogo} width={200} ></img> : null}
                         </div>
-                    </li>
-                    <Link to="/our-story">Our Story</Link>
-                    <Link to="/custom">Custom</Link>
-                    <Link to="/contact">Contact</Link>
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Signup</Link>
-                </nav>
-                <hr></hr>
-            </div>
+                        <div className={classes.NavbarMiddle}>
+                            <Link to="/">Home</Link>
+                            <span className="nav-item dropdown" onMouseOver={this.handleOnHover} >
+                                <Link className="dropdown-toggle"
+                                      href="/"
+                                      id="navbarDropdownMenuLink"
+                                      to="/"
+                                      data-toggle="dropdown"
+                                      aria-haspopup="true"
+                                      aria-expanded="false">Jewelery</Link>
+
+                                <span className={this.state.isHovered ? "dropdown-menu show" : "dropdown-menu"}
+                                      aria-labelledby="navbarDropdownMenuLink"
+                                      onMouseOver={this.handleOnHover}
+                                      onMouseLeave={this.handleOffHover}
+                                >
+                                    {this.vendorDropDowns}
+                                </span>
+                            </span>
+                            <Link to="/our-story">Our Story</Link>
+                            <Link to="/custom">Custom</Link>
+                            <Link to="/contact">Contact</Link>
+                        </div>
+                        <div className={classes.NavbarRight}>
+                            <Link to="/login">Login</Link>
+                            <Link to="/signup">Signup</Link>
+                        </div>
+                    </nav>
+                  <hr ></hr>
+                 </div>
 
         );
     }
