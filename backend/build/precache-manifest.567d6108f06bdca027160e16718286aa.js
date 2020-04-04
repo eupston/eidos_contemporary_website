01931ee681999d51ae1d8e3e8cb39ed5a311e61b
eupston <eupston130@hotmail.com>
2020-04-05 11:13:43 +1200

    #59 more build file

---
+++ backend/build/precache-manifest.567d6108f06bdca027160e16718286aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8164c7901823079a06fe7f62895f63b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e3d0df0cfb23163f756",
+    "url": "/static/css/main.2ea22276.chunk.css"
+  },
+  {
+    "revision": "693dd9f5445958047c93",
+    "url": "/static/js/2.0c486c50.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.0c486c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e3d0df0cfb23163f756",
+    "url": "/static/js/main.4c1707b5.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  }
+]);
